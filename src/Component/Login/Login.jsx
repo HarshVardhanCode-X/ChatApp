ca809985052523d1dce42c6ea7dd70ea0a4856cc
Harsh Vardhan <harshvardhan232002@gmail.com>
2024-10-10 09:21:11 +0530

    13 th!

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { auth, provider } from "../../firebaseConfig";
-import { GithubAuthProvider, signInWithPopup, signInWithRedirect, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, GithubAuthProvider, signInWithEmailAndPassword, signInWithPopup, signInWithRedirect, signOut } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
 import { ImGithub } from "react-icons/im";
 import { BsLinkedin } from "react-icons/bs";
@@ -12,6 +12,32 @@ function Login() {
 
 
     const [user, setUser] = useState(null);
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+
+    const signInWithMail = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, email,password);
+            window.location.assign("/Home");
+            
+        } catch (error) {
+            console.error("Error signing in with Google", error);
+        }
+    };
+
+    const signInWithMailCreate = async (e) => {
+        e.preventDefault()
+        try {
+            await createUserWithEmailAndPassword(auth, email,password);
+            window.location.assign("/Home");
+            
+        } catch (error) {
+            console.error("Error signing in with Google", error);
+            signInWithMail();
+        }
+    };
+
+
     const signInWithGoogle = async () => {
         try {
             const result = await signInWithPopup(auth, provider);
@@ -58,7 +84,7 @@ function Login() {
                     
                     <div className="w-full md:w-1/2 p-8">
                         <div className="text-right">
-                            <p className="text-gray-500 text-sm">
+                            <p className="text-gray-500 text-sm" >
                                 Not a member? <a href="#" className="text-blue-500">Register now</a>
                             </p>
                         </div>
@@ -68,24 +94,25 @@ function Login() {
                         <form>
                             <div className="mb-4">
                                 <input
-                                    type="text"
+                                    type="email"
                                     placeholder="Enter username"
                                     className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                />
+                                onChange={(e)=>setEmail(e.target.value)}/>
                             </div>
                             <div className="input-box1 mb-4">
                                 <input
                                     type="password"
                                     placeholder="Password"
                                     className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                />
+                                    onChange={(e)=>setPassword(e.target.value)}/>
                                 <div className="text-right mt-2" >
                                     <a href="#" className="text-sm text-blue-500" >Recovery Password</a>
                                 </div>
                             </div>
                             
                             <div>
-                            <button className="w-full py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300">
+                            <button className="w-full py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300"
+                            onClick={signInWithMailCreate}>
                                 Sign In
                             </button>
                             </div>
